0485d93b3167f8366ae10a692cd189d5e6beabb3
Yonghyun Kim (Freddy) <yhkks1038@gmail.com>
2021-09-15 20:05:29 +0900

    CORS 이슈 해결

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -1,4 +1,9 @@
 module Api
   class BaseController < ActionController::API
+    before_action :access_control_allow_origin
+
+    def access_control_allow_origin
+      response.set_header 'Access-Control-Allow-Origin', '*'
+    end
   end
 end
